80295b89e896da6560a48166449543c17e318d91
Vaernion <vaporz@gmail.com>
2020-06-21 02:40:14 +0200

    optional grid size

--- tictac.py
+++ tictac.py
@@ -5,95 +5,133 @@ def clearScreen():
     os.system("cls" if os.name == "nt" else "clear")
 
 
-class TicTac:
-    "X = 1, O = 2, X goes first"
-
-    def __init__(self):
-        self.board = [0] * 9
-        self.winner = 0
-        self.xIsActive = True
-        self.turn = 1
-        self.lastMove = ""
-
-
-def printBoard(game):
-    clearScreen()
-    print("Tic-Tac-Toe")
-    for i, v in enumerate(game.board):
-        filler = " "
-        if v == 1:
-            filler = "X"
-        elif v == 2:
-            filler = "O"
-        print(f"[{filler}]", end="")
-        if (i + 1) % 3 == 0:
-            print("")
-    print(game.lastMove)
-
-
 def inputIntInRange(text, x, y):
     while 1:
         try:
             num = int(input(text))
+            if not x <= num <= y:
+                raise Exception
         except:
             pass
         else:
-            return num if x <= num <= y else 0
+            return num
 
 
-def playerTurn(game):
-    pos = 0
+class TicTac:
+    "X = 1, O = 2, X goes first"
 
-    while not (1 <= pos <= 9):
-        text = f"Place {'X' if game.xIsActive else 'O' }, input 1-9: "
-        pos = inputIntInRange(text, 1, 9)
-        if game.board[pos - 1] != 0:
-            print(f"Square {pos} is not free")
-            pos = 0
-            continue
-    else:
-        game.board[pos - 1] = 1 if game.xIsActive else 2
-        game.lastMove = f"{'X' if game.xIsActive else 'O'} placed on {pos}"
-        game.xIsActive = not game.xIsActive
-        game.turn += 1
+    def __init__(self, grid=3):
+        self.grid = grid
+        self.squares = grid ** 2
+        self.board = [0] * self.squares
+        self.winner = 0
+        self.xIsActive = True
+        self.turn = 1
+        self.lastMove = ""
+        self.combos = TicTac.generateCombos(grid)
 
+    def printBoard(self):
+        clearScreen()
+        # print(self.combos)
+        print("Tic-Tac-Toe")
 
-def findWinner(game):
-    winner = 0
-    board = game.board
-    combos = (
-        (0, 1, 2),
-        (3, 4, 5),
-        (6, 7, 8),
-        (0, 3, 6),
-        (1, 4, 7),
-        (2, 5, 8),
-        (0, 4, 8),
-        (2, 4, 6),
-    )
+        for i, v in enumerate(self.board):
+            filler = " "
+            if v == 1:
+                filler = "X"
+            elif v == 2:
+                filler = "O"
+            print(f"[{filler}]", end="")
+            if (i + 1) % self.grid == 0:
+                print("")
 
-    for combo in combos:
-        if (
-            board[combo[0]] == board[combo[1]] == board[combo[2]]
-            and board[combo[0]] != 0
-        ):
-            winner = board[combo[0]]
-            break
-    return winner
+        print(self.lastMove)
+
+    def playerTurn(self):
+        pos = 0
+        while not (1 <= pos <= self.squares):
+            text = f"Place {'X' if self.xIsActive else 'O' }, input 1-{self.squares}: "
+            pos = inputIntInRange(text, 1, self.squares)
+            if self.board[pos - 1] != 0:
+                print(f"Square {pos} is not free")
+                pos = 0
+                continue
+        else:
+            self.board[pos - 1] = 1 if self.xIsActive else 2
+            self.lastMove = f"{'X' if self.xIsActive else 'O'} placed on {pos}"
+            self.xIsActive = not self.xIsActive
+            self.turn += 1
+
+    def findWinner(self):
+        winner = 0
+        board = self.board
+
+        for combo in self.combos:
+            if self.findWinnerHelper(combo) and board[combo[0]] != 0:
+                winner = board[combo[0]]
+                break
+
+        return winner
+
+    def findWinnerHelper(self, combo):
+        if self.grid == 3:
+            return self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]]
+        elif self.grid == 4:
+            return (
+                self.board[combo[0]]
+                == self.board[combo[1]]
+                == self.board[combo[2]]
+                == self.board[combo[3]]
+            )
+        elif self.grid == 5:
+            return (
+                self.board[combo[0]]
+                == self.board[combo[1]]
+                == self.board[combo[2]]
+                == self.board[combo[3]]
+                == self.board[combo[4]]
+            )
+
+    @staticmethod
+    def generateCombos(grid):
+        combos = []
+        for n in range(grid):
+            start = n * grid
+            combos.append(TicTac.comboGeneratorHelper(grid, start, n, "hor"))
+            combos.append(TicTac.comboGeneratorHelper(grid, start, n, "vert"))
+        combos.append(TicTac.comboGeneratorHelper(grid, start, n, "diagTopLeft"))
+        combos.append(TicTac.comboGeneratorHelper(grid, start, n, "diagTopRight"))
+        return combos
+
+    @staticmethod
+    def comboGeneratorHelper(grid, start, n, direction):
+        combo = []
+        for i in range(grid):
+            if direction == "hor":
+                combo.append(start + i)
+            elif direction == "vert":
+                combo.append(grid * i + n)
+            elif direction == "diagTopLeft":
+                combo.append((grid + 1) * i)
+            elif direction == "diagTopRight":
+                combo.append((grid - 1) * (i + 1))
+        return combo
 
 
 def main():
-    game = TicTac()
+    clearScreen()
+    game = TicTac(inputIntInRange("Enter grid size from 3 to 5: ", 3, 5))
 
     while game.winner == 0:
-        printBoard(game)
-        if game.turn > 9:
+        game.printBoard()
+
+        if game.turn > game.squares:
             print("Board is full, game is a draw")
             break
-        playerTurn(game)
-        game.winner = findWinner(game)
+        game.playerTurn()
+        game.winner = game.findWinner()
     else:
-        printBoard(game)
+        game.printBoard()
         print(f"{'X' if game.winner == 1 else 'O'} won the game!")
 
 
